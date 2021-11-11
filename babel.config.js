2fff6dcb4c20b94b38e5291b41e12365f5c61d38
Max Rusakovič <maxrusakovic@gmail.com>
2021-11-11 10:03:26 +0100

    [2] Added babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@types': './src/types',
+          '@routes': './src/routes',
+          '@constants': './src/constants',
+        },
+      },
+    ],
+  ],
 };
